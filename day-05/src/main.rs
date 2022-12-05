881758633a7c148780e8da15a3540907dddd0461
jokr-1 <johannes.krimphove@rwth-aachen.de>
2022-12-05 15:36:46 +0100

    little change

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,9 +1,29 @@
 use std::{collections::LinkedList, vec};
 
-struct Command {
-    from: usize,
-    to: usize,
-    count: usize,
+fn main() {
+    let input = include_str!("../input");
+    let (stack_def, cmds_def) = input.split_once("\n\n").unwrap();
+
+    let cmds = cmds_def.trim().lines().map(|line| {
+        let vals: Vec<usize> = line
+            .split_whitespace()
+            .skip(1)
+            .step_by(2)
+            .map(|v| v.parse().unwrap())
+            .collect(); // count, from, to
+        (vals[0], vals[1] - 1, vals[2] - 1)
+    });
+
+    let mut stack_a = Stack::from_str(stack_def);
+    let mut stack_b = Stack::from_str(stack_def);
+
+    for (count, from, to) in cmds {
+        stack_a.move_item(from, to, count);
+        stack_b.shift_item(from, to, count);
+    }
+
+    println!("Part 1: {}", stack_a.top());
+    println!("Part 2: {}", stack_b.top());
 }
 
 struct Stack {
@@ -40,41 +60,4 @@ impl Stack {
     fn top(self) -> String {
         self.data.iter().map(|l| l.back().unwrap()).collect()
     }
-}
-
-fn main() {
-    let input = include_str!("../input");
-    let (stack_def, cmds_def) = input.split_once("\n\n").unwrap();
-
-    let cmds: Vec<Command> = cmds_def
-        .trim()
-        .lines()
-        .map(|line| {
-            let vals: Vec<usize> = line
-                .split_whitespace()
-                .skip(1)
-                .step_by(2)
-                .map(|v| v.parse().unwrap())
-                .collect(); // count, from, to
-            Command {
-                count: vals[0],
-                from: vals[1] - 1,
-                to: vals[2] - 1,
-            }
-        })
-        .collect();
-
-    // part 1
-    let mut stack = Stack::from_str(stack_def);
-    for cmd in cmds.iter() {
-        stack.move_item(cmd.from, cmd.to, cmd.count)
-    }
-    println!("Part 1: {}", stack.top());
-
-    // part 2
-    let mut stack = Stack::from_str(stack_def);
-    for cmd in cmds.iter() {
-        stack.shift_item(cmd.from, cmd.to, cmd.count)
-    }
-    println!("Part 2: {}", stack.top());
 }
