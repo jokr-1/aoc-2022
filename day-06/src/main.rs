e68119b17e086022d42722297ee412f69b407da5
jokr-1 <johannes.krimphove@rwth-aachen.de>
2022-12-06 13:52:20 +0100

    little change

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,19 +1,18 @@
 use itertools::Itertools;
 
 fn main() {
-    let input = include_str!("../input").trim();
-    println!("Part 1: {:?}", find(input, 4));
-    println!("Part 2: {:?}", find(input, 14));
+    let input = include_str!("../input");
+
+    println!("Part 1: {:?}", find_start(input, 4));
+    println!("Part 2: {:?}", find_start(input, 14));
 }
 
-fn find(input: &str, windowsize: usize) -> usize {
+fn find_start(input: &str, windowsize: usize) -> usize {
     input
         .chars()
         .collect::<Vec<char>>()
         .windows(windowsize)
-        .enumerate()
-        .find(|(_, w)| w.iter().unique().count() == windowsize)
+        .position(|window| window.iter().all_unique())
         .unwrap()
-        .0
         + windowsize
 }
